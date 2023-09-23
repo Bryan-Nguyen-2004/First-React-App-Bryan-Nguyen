87e9b61c6803d90d094103e4c1352ff95a2f2cd5
Bryan Nguyen <bryanisaw350m3@gmail.com>
2023-09-23 14:12:09 -0700

    added state

--- src/Table.js
+++ src/Table.js
@@ -5,6 +5,7 @@ function TableHeader() {
         <tr>
           <th>Name</th>
           <th>Job</th>
+          <th>Remove</th>
         </tr>
       </thead>
     );
@@ -16,6 +17,12 @@ function TableHeader() {
         <tr key={index}>
           <td>{row.name}</td>
           <td>{row.job}</td>
+          <td>
+            <button onClick={() => 
+              props.removeCharacter(index)}>
+              Delete
+            </button>
+          </td>
         </tr>
       );
      }
@@ -27,12 +34,13 @@ function TableHeader() {
      );
   }
   
-  function Table(props) {
-      return (
-        <table>
-          <TableHeader />
-          <TableBody characterData={props.characterData} />
-        </table>
-      );
+  function Table (props) {
+    return (
+      <table>
+        <TableHeader />
+        <TableBody characterData={props.characterData} 
+          removeCharacter={props.removeCharacter} />
+      </table>
+    );
   }
   export default Table;
